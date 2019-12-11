432b67ece8f8599dc70c460714b11db78123c312
gabriel <gabionezero@gmail.com>
2019-12-11 13:59:08 +0100

    Crea método resta

--- src/main/java/com/mycompany/racionalgabrielalberto/Racional.java
+++ src/main/java/com/mycompany/racionalgabrielalberto/Racional.java
@@ -5,45 +5,59 @@ package com.mycompany.racionalgabrielalberto;
  * @author gabriel
  */
 public class Racional {
-    
+
     private int numerador;
     private int denominador;
 
     public Racional() {
         this.numerador = 1;
-        this.denominador =1;
+        this.denominador = 1;
     }
 
     public Racional(int numerador, int denominador) {
         this.numerador = numerador;
-        
-        
-        if (denominador == 0){
+
+        if (denominador == 0) {
             this.denominador = 1;
             System.out.println("El denominador no es valido, se asigna el valor 1");
-            
-        }else{
+
+        } else {
             this.denominador = denominador;
-            
-        }        
+
+        }
     }
-    
-    public void suma(Racional racional){
-        
-        if (this.denominador == racional.denominador){
-            
+
+    public void suma(Racional racional) {
+
+        if (this.denominador == racional.denominador) {
+
             this.numerador += racional.numerador;
-            
-        }else{
-            
-            this.numerador = (this.numerador * racional.denominador) 
-            + (this.denominador * racional.numerador);
-            
+
+        } else {
+
+            this.numerador = (this.numerador * racional.denominador)
+                    + (this.denominador * racional.numerador);
+
             this.denominador *= racional.denominador;
         }
-       
-            
+
+    }
+    
+    public void resta(Racional racional) {
+
+        if (this.denominador == racional.denominador) {
+
+            this.numerador -= racional.numerador;
+
+        } else {
+
+            this.numerador = (this.numerador * racional.denominador)
+                    - (this.denominador * racional.numerador);
+
+            this.denominador *= racional.denominador;
         }
+
+    }
 
     public int getNumerador() {
         return numerador;
@@ -58,29 +72,23 @@ public class Racional {
     }
 
     public void setDenominador(int denominador) {
-        if (denominador == 0){
+        if (denominador == 0) {
             this.denominador = 1;
             System.out.println("El denominador no es valido, se asigna el valor 1");
-            
-        }else{
+
+        } else {
             this.denominador = denominador;
-            
+
         }
     }
-    
-    public void imprimirConsola(){
+
+    public void imprimirConsola() {
         System.out.println("Número racional" + numerador + "/" + denominador);
     }
 
     @Override
     public String toString() {
-        return  numerador + "/" + denominador;
+        return numerador + "/" + denominador;
     }
-    
-    
-    
-    
-    
-    
-    
+
 }
