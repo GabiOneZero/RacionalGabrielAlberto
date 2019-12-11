9e09a0686732e423fc3fb16105830bd857ad5626
gabriel <gabionezero@gmail.com>
2019-12-11 13:25:06 +0100

    Añade parámetros y constructor por defecto

--- src/main/java/com/mycompany/racionalgabrielalberto/Racional.java
+++ src/main/java/com/mycompany/racionalgabrielalberto/Racional.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package com.mycompany.racionalgabrielalberto;
 
 /**
@@ -10,5 +5,15 @@ package com.mycompany.racionalgabrielalberto;
  * @author gabriel
  */
 public class Racional {
+    
+    private int numerador;
+    private int denominador;
+
+    public Racional() {
+        this.numerador = 1;
+        this.denominador =1;
+    }
+    
+    
     
 }
